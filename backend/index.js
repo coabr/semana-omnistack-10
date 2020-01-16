07b4329f4ae25f8571a980186eaadc6b7fa9242d
COAB1 <carol.o.ag@gmail.com>
2020-01-16 19:48:11 -0300

    Including backend

---
+++ backend/index.js
@@ -0,0 +1,5 @@
+const express = require('express'); /* importando o módulo express*/
+
+const app = express(); /*variável que não precisará ser mudada, é uma função */
+
+app.listen(3333); /*escolher a porta do local host*/
